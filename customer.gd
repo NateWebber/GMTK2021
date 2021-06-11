24cd6bfa04ab1c73af2104c756207de13f905955
NateWebber <nwebber@u.rochester.edu>
2021-06-11 15:33:44 -0400

    random "customer" generation

---
+++ customer.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+var gender_id
+var hair_id
+var skin_id
+var flower_id
+
+var partner_id
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+func set_trait_ids(new_gender, new_hair, new_skin, new_flower):
+	gender_id = new_gender
+	hair_id = new_hair
+	skin_id = new_skin
+	flower_id = new_flower
+
+func get_trait_ids():
+	var trait_ids = []
+	trait_ids.append(gender_id)
+	trait_ids.append(hair_id)
+	trait_ids.append(skin_id)
+	trait_ids.append(flower_id)
+	return trait_ids
+	
+func set_appearance():
+	$gender_id_label.text = str(gender_id)
+	$hair_id_label.text = str(hair_id)
+	$skin_id_label.text = str(skin_id)
+	$flower_id_label.text = str(flower_id)
---
+++ scenes/main.gd
@@ -0,0 +1,74 @@
+extends Node2D
+
+var rng = RandomNumberGenerator.new()
+
+var customer = preload("res://scenes/customer.tscn")
+
+const GENDER_VARIANTS = 2
+const HAIR_VARIANTS = 5
+const SKIN_VARIANTS = 5
+const FLOWER_VARIANTS = 5
+
+var current_customers = []
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rng.randomize()
+	setup_new_game()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _input(event):
+	if event is InputEventMouseButton:
+		generate_new_customer()
+
+func setup_new_game():
+	current_customers = []
+
+func generate_new_customer_traits():
+	print("Generating new customer traits!")
+	var new_traits = []
+	new_traits.append(rng.randi_range(1, GENDER_VARIANTS))
+	new_traits.append(rng.randi_range(1, HAIR_VARIANTS))
+	new_traits.append(rng.randi_range(1, SKIN_VARIANTS))
+	new_traits.append(rng.randi_range(1, FLOWER_VARIANTS))
+	print("Generated this set of traits:")
+	print(new_traits)
+	return new_traits
+
+func check_traits_duplicated(trait_set_1, trait_set_2):
+	var gender_matches = trait_set_1[0] == trait_set_2[0]
+	var hair_matches = trait_set_1[1] == trait_set_2[1]
+	var skin_matches = trait_set_1[2] == trait_set_2[2]
+	var flower_matches = trait_set_1[3] == trait_set_2[3]
+	return gender_matches and hair_matches and skin_matches and flower_matches
+
+func generate_new_customer():
+	print("Starting to generate a new customer!")
+	var traits = generate_new_customer_traits()
+	var found_match = false
+	for existing_traits in current_customers:
+		if check_traits_duplicated(traits, existing_traits):
+			found_match = true
+			print("Newly generated customer matches an existing one!")
+			break
+	if found_match:
+		generate_new_customer()
+		print("Generating again since we found a match!")
+	else:
+		print("Adding a customer with these traits:")
+		print(traits)
+		var new_customer = customer.instance()
+		new_customer.set_trait_ids(traits[0], traits[1], traits[2], traits[3])
+		new_customer.set_appearance()
+		new_customer.position = get_global_mouse_position()
+		add_child(new_customer)
